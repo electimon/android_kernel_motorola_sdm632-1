3335a73a6abcd8c32d602a7c6b5960cc40495a85
daiss1 <daiss1@motorola.com>
2017-03-20 11:21:54 +0800

    msm: mdss: Add new no_panel_on_read_support flag
    Cherry Pick msm8953 https://gerrit.mot.com/#/c/916784/
    
    Because synaptics TD4300 needs to wait for some time to read pwr
    mode register, it's not compatible with current display driver.
    So add new no_panel_on_read_support display flag to indicate if
    this panel supports pwr mode register read request when screen on.
    
    Change-Id: I50f316229e33eeb85af64b4e976e5358639a43bf
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1165998
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Shanshan Dai <daiss1@lenovo.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -1130,9 +1130,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	/* Ensure low persistence mode is set as before */
 	mdss_dsi_panel_apply_display_setting(pdata, pinfo->persist_mode);
 
-	mdss_dsi_get_pwr_mode(pdata, &pwr_mode, false);
-	if (pinfo->disp_on_check_val != pwr_mode) {
-		pr_err("%s: Display failure: read = 0x%x, expected = 0x%x\n",
+	if (pinfo->no_panel_on_read_support == false) {
+		mdss_dsi_get_pwr_mode(pdata, &pwr_mode, false);
+		if (pinfo->disp_on_check_val != pwr_mode)
+			pr_err("%s: Display failure: read = 0x%x, expected = 0x%x\n",
 				__func__, pwr_mode, pinfo->disp_on_check_val);
 
 		if (pdata->panel_info.panel_dead)
@@ -2506,6 +2507,9 @@ static int mdss_dsi_parse_panel_features(struct device_node *np,
 		else
 			pinfo->disp_on_check_val = 0x9c;
 	}
+
+	pinfo->no_panel_on_read_support = of_property_read_bool(np,
+					"qcom,mdss-dsi-no-panel-on-read-support");
 
 	if (ctrl->disp_en_gpio <= 0) {
 		ctrl->disp_en_gpio = of_get_named_gpio(
