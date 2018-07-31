f048246f7f926191c65cb86437c39ccfb52076d4
Haijian Ma <mahj8@motorola.com>
2018-07-31 14:19:07 +0800

    power: smblib: Add Age
    Make the Battery Age available.
    
    Porting from
    https://gerrit.mot.com/993904
    https://gerrit.mot.com/#/c/993903/
    
    Change-Id: If726235a24b3d287fa34c4c30605cf0ce81c11a6
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1218037
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1286,6 +1286,7 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 	POWER_SUPPLY_PROP_CHARGE_RATE,
+	POWER_SUPPLY_PROP_AGE,
 };
 
 #define ITERM_SCALING_FACTOR_PMI632	1525
@@ -1462,6 +1463,9 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_RATE:
 		mmi_chrg_rate_check(chg);
 		val->intval = chg->mmi.charger_rate;
+		break;
+	case POWER_SUPPLY_PROP_AGE:
+		rc = smblib_get_prop_batt_age(chg, val);
 		break;
 	default:
 		pr_err("batt power supply prop %d not supported\n", psp);
