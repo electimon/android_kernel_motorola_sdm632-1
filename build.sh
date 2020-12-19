11116baabcdbcda437debe7b3c96abd477c77e1a
electimon <electimon@gmail.com>
2020-12-19 21:37:00 +0000

    build.sh: Rework error handling

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Setup Variables
 export ARCH=arm64
 export CROSS_COMPILE=/srv/root/build/android/rvgR/prebuilts/gcc/linux-x86/aarch64/arm64-gcc/bin/aarch64-elf-
@@ -8,6 +9,28 @@ cd `dirname $0`
 export KERNDIR=`pwd`
 export AZURREVISION=${KERNDIR: -1}
 
+# Setup Functions
+needExitCheck () {
+	if [[ $NEEDSEXIT == "1" ]]; then
+		echo "Well well well, looks like something went wrong..."
+		if [[ `cat build.log` == *"default configuration"* ]]; then
+			exit 1
+		else
+			exit 0
+		fi
+	fi
+}
+
+# Generate defconfig
+make ${DEVICE}_defconfig &> build.log || export NEEDSEXIT=1
+
+needExitCheck
+
+# Build the kernel!
+make &> build.log || export NEEDSEXIT=1
+
+needExitCheck
+
 # Setup revision control
 if [[ -f ${DEVICE}revision ]]; then
         OLDRE=`cat ${DEVICE}revision`
@@ -16,18 +39,6 @@ else
 	echo 1 > ${DEVICE}revision
 fi
 export KERNELREVISION=`cat ${DEVICE}revision`
-
-# Generate defconfig
-make ${DEVICE}_defconfig
-
-# Build the kernel!
-# Generate a log file for ez debugging and exit upon error.
-make || export NEEDSEXIT=1
-
-if [[ $NEEDSEXIT == "1" ]]; then
-echo "Well well well, looks like something went wrong..."
-exit 1
-fi
 
 # Obtain the goodness
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3${DEVICE}
